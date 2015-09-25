b870bb62d967b105cf633d2de2fcd8c402866fba
vladimiriii <brett.romero@hotmail.com>
2015-09-25 09:11:02 +0200

    Add script for assignment submissions

--- Assignment/additional_code.R
+++ Assignment/additional_code.R
@@ -98,3 +98,15 @@ pcaMNModelFit <- train(classe ~ .
                        , trace = FALSE #multinom prints a lot of output if TRUE
 )
 pcaMNModelFit
+
+# File Creation
+predictions <- c("B", "A", "B", "A", "A", "E", "D", "B", "A", "A", "B", "C", "B", "A", "E", "E", "A", "B", "B", "B")
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+pml_write_files(predictions)
+
